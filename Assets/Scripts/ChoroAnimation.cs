81751e3993e4df82254d5734f7fa70cb67d3f6b9
kero <kero@comore.bi>
2020-09-04 10:09:33 +0900

    キャラクタを操作可能に変更

--- Assets/Scripts/ChoroAnimation.cs
+++ Assets/Scripts/ChoroAnimation.cs
@@ -11,14 +11,14 @@ public class ChoroAnimation : MonoBehaviour {
   }
 
   private void Update() {
-    var time = Time.time / 1.5f;
-    var deg = time * Mathf.Rad2Deg % 360;
-    var scale = transform.localScale;
-    var scaleXAbs = Mathf.Abs(scale.x);
-
-    transform.position = new Vector2(Mathf.Sin(time) * 60.0f + _position.x, _position.y);
-
-    scale.x = deg > 90 && deg < 270 ? scaleXAbs : -scaleXAbs;
-    transform.localScale = scale;
+    // var time = Time.time / 1.5f;
+    // var deg = time * Mathf.Rad2Deg % 360;
+    // var scale = transform.localScale;
+    // var scaleXAbs = Mathf.Abs(scale.x);
+    //
+    // transform.position = new Vector2(Mathf.Sin(time) * 60.0f + _position.x, _position.y);
+    //
+    // scale.x = deg > 90 && deg < 270 ? scaleXAbs : -scaleXAbs;
+    // transform.localScale = scale;
   }
 }
